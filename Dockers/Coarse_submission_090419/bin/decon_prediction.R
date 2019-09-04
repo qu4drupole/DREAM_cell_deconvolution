eff24c7c57dcefaecbd0eaf042d6a0c935449579
lowrylab-cuboulder <34254263+lowrylab-cuboulder@users.noreply.github.com>
2019-09-04 15:57:46 -0400

    new docker
    new docker

---
+++ Dockers/Coarse_submission_090419/bin/decon_prediction.R
@@ -0,0 +1,58 @@
+####################################################
+# PRIMARY SCRIPT TO RUN DECONVOLUTION PREDICTIONS
+#
+# REQUIRES: 
+#   'coarse_cell_data_models.RData' -- cell models and dfs
+#   'functions.R' -- processing and formatting functions
+#
+####################################################
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(purrr)
+library(tibble)
+library(reshape2)
+
+source(functions.R)
+load("coarse_cell_models_list.RData")
+
+# read meta data
+input_df <- readr::read_csv("input/input.csv")
+dataset_names <- input_df$dataset.name
+expression_files  <- input_df$hugo.expr.file
+expression_paths <- paste0("input/", expression_files)
+data_scales <- input_df$scale
+
+# need to nest module df and cell model in list
+# e.g. cell_model = list('df'=cd4_mod_df, 'model' = cd4_model)
+expression_path <- expression_paths[1]
+dataset_name <- dataset_names[1]
+data_scale <- data_scales[1]
+
+
+do_decon <- function(expression_path, dataset_name, data_scale){
+  #format the data
+  ex <- expression_path %>%
+    readr::read_csv() %>%
+    as.data.frame() %>%
+    tibble::column_to_rownames("Gene") %>%
+    format_data(scale = data_scale)
+  
+  #calculate distance, rotation, and make predictions
+  pred_matrix <- sapply(cell_models, function(x) make_prediction(x, ex))
+  pred_df <- as.data.frame(pred_matrix)
+  pred_df <- mutate(pred_df, 'sample.id' = rownames(ex), 'dataset.name' = dataset_name)
+  pred_df <- melt(pred_df, id.vars = c('dataset.name', 'sample.id'), variable.name = 'cell.type', 
+                  value.name = 'measured') %>%
+    arrange(sample.id)
+  return(pred_df)
+}
+
+test_res <- pmap(list(expression_paths, dataset_names, data_scales), do_decon)
+
+combined_result_df <- dplyr::bind_rows(test_res)
+
+dir.create("output")
+readr::write_csv(combined_result_df, "output/predictions.csv")
+
---
+++ Dockers/Coarse_submission_090419/bin/functions.R
@@ -0,0 +1,50 @@
+# FUNCTIONS USED IN 'decon_prediction.R'
+
+format_data <- function(test_df, scale){
+  #scale test_df
+  if(scale == 'Linear'){
+    test_df = log2(test_df)
+  }else if(scale == 'Log10'){
+    test_df = test_df*log2(10)
+  }
+  #impute NAs
+  na.rows <- unname(which(apply(test_df, 1, function(x) sum(is.na(x))>0)))
+  for(r in na.rows){
+    test_df[r,is.na(test_df[r,])] <- median(unlist(test_df[r,]), na.rm = T)
+  }
+  #format
+  test_df = t(test_df)
+  mix_cols = colnames(test_df)[colnames(test_df) %in% colnames(coarse_df)]
+  test_df = test_df[,mix_cols]
+  missing_genes = colnames(coarse_df)[(!(colnames(coarse_df) %in% colnames(test_df)))]
+  median_expression = apply(test_df,1,median)
+  missing_genes_df = sapply(missing_genes, function(g) median_expression, simplify = 'array')
+  test_df_all = cbind(test_df, missing_genes_df)
+  test_df_all = test_df_all[,colnames(coarse_df)]
+  return(test_df_all)
+}
+
+make_prediction <- function(cell_model, test_df){
+  # browser()
+  test_df_mod = test_df[,colnames(cell_model$df)]
+  test_dist = array(matrix(100, nrow = ncol(test_df_mod), ncol = ncol(test_df_mod)),
+                      dim = c(ncol(test_df_mod),ncol(test_df_mod),nrow(test_df_mod)))
+  for(i in 1:nrow(test_df_mod)){
+    gene_ME_dist = matrix(100, nrow = ncol(test_df_mod), ncol = ncol(test_df_mod))
+    for(j in 1:ncol(test_df_mod)){
+      ME_dist = sapply(unlist(test_df_mod[i,]), function(x) (x-test_df_mod[i,j])^2)
+      gene_ME_dist[j,] = ME_dist
+    }
+    test_dist[,,i] <- gene_ME_dist
+  }
+  
+  test_rotation = t(sapply(1:dim(test_dist)[3], function(i) svd(test_dist[,,i])$u[,1]))
+  
+  test_pred <- predict(cell_model$model, s=cell_model$model$lambda.1se, newx=test_rotation)
+  # print('done')
+  return(test_pred)
+}
+
+
+
+
